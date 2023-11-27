407fb98bcbbe0bb7d6e4a68464eb5c08bae40af8
zMaxiFantasti <schmidt.maxi2001@gmail.com>
2023-11-27 16:17:05 +0100

    jj

--- 07_Termin7/noteverw.c
+++ 07_Termin7/noteverw.c
@@ -7,11 +7,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct liste{
-    char Vorname[10];
-    char Nachname[10];
+typedef struct liste
+{
+    char Vorname[25];
+    char Nachname[25];
     int note;
-}List;
+} List;
 
 int main()
 {
@@ -19,21 +20,27 @@ int main()
     int durchschnitt = 0;
     printf("----Eingabe des 1. Schuelers----\n");
     List Schueler[10];
-    printf("\nName:\t\t");
-    while(scanf("%s", Schueler[i].Nachname) == 1)
-        {
-          scanf("\n Vorname: \t\t%s\n Note:\t\t%d\n",Schueler[i].Vorname, &Schueler[i].note);
-            i++;
-            printf("----Eingabe des %d. Schuelers----",i);
-            durchschnitt += Schueler[i].note;
-        }
-    //Tabellenausgabe
-    printf("\t Name \t\t, Vorname \t\t , Note \n--------------------------------------------------------");
-    for( int j = 0; j<=i; j++)
-        {
-            printf("%d . %s \t\t, %s \t\t , %d\n ", j, Schueler[j].Nachname, Schueler[j].Vorname, Schueler[j].note );
-        }
-    if(i > 0)
+    printf("Name:");
+    fgets(Schueler[0].Nachname, 25, stdin);
+    while (Schueler[i].Nachname[0] != '\n')
+    {
+        printf("\nVorname:");
+        fgets(Schueler[i].Vorname, 25, stdin);
+        printf("\nNote:");
+        scanf("%d", &Schueler[i].note);
+        durchschnitt += Schueler[i].note;
+        i++;
+        printf("----Eingabe des %d. Schuelers----\n", (i+1));
+        fgets(Schueler[i].Nachname, 25, stdin);
+        // while(getchar());
+    }
+    // Tabellenausgabe
+    printf("\t Name \t\t, Vorname \t\t , Note \n--------------------------------------------------------\n");
+    for (int j = 0; j <= i; j++)
+    {
+        printf("%d . %s \t\t\t, %s \t\t , %d\n ", (j+1), Schueler[j].Nachname, Schueler[j].Vorname, Schueler[j].note);
+    }
+    if (i > 0)
     {
         durchschnitt /= i;
         printf("Durchschnittsnote: %d\n", durchschnitt);
