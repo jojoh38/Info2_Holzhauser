0390052155031045683d47602d3468486cd6530c
zMaxiFantasti <schmidt.maxi2001@gmail.com>
2023-11-27 12:20:36 +0100

    Beginn

---
+++ 07_Termin7/noteverw.c
@@ -0,0 +1,15 @@
+/*****************************************************************
+ * Aufgabe 28_4_4 Notenübersicht                      27.11.2023 *
+ * Johannes Holzhauser und Maximilian Schmidt                    *
+ *****************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct liste{
+    char Vorname[10];
+    char Nachname[10];
+    
+
+}
