a72e2754333f11bb448c686d8506878648f5ad1b
jojoh38 <jholzhauser@gmx.de>
2023-12-01 12:01:05 +0100

    MA3_Form

--- 07_Termin7/ModulAufgabe3/queue.c
+++ 07_Termin7/ModulAufgabe3/queue.c
@@ -6,7 +6,7 @@
 * 25.11.2023 Johannes Holzhauser und Maximilian Schmidt
 *
 \**********************************************************************/
- 
+
 /*--- #includes ------------------------------------------------------*/
 #include "queue.h"
 #include <stdio.h>
@@ -15,75 +15,74 @@
 #define SPEICHER sizeof(List)
 /*--- Datentypen (typedef) -------------------------------------------*/
 /*--- Prototypen globaler Funktionen ---------------------------------*/
- 
-typedef struct liste {
+
+typedef struct liste
+{
 	int nummer;
 	struct liste *naechste;
-}List;
- 
+} List;
+
 static List *anfang = NULL;
 static List *ende = NULL;
- 
+
 /*--------------------------------------------------------------------*\
-* Einfuegen in Liste
-* Parameter:
-*    zahl  fügt die übergebene Zahl 'zahl' am Ende der Liste ein
-* Return Wert:
-*    TRUE  wenn noch genug Speicherplatz vorhanden
-*    FALSE wenn kein Speicherplatz mehr allokiert werden konnte
-* Hinweis:
-* - dynamische Verwaltung von Speicher muss mit malloc() und free()
-*   durchgeführt werden; dynamische Arrays sind nicht erlaubt !!!
-*--------------------------------------------------------------------*/
+ * Einfuegen in Liste
+ * Parameter:
+ *    zahl  fügt die übergebene Zahl 'zahl' am Ende der Liste ein
+ * Return Wert:
+ *    TRUE  wenn noch genug Speicherplatz vorhanden
+ *    FALSE wenn kein Speicherplatz mehr allokiert werden konnte
+ * Hinweis:
+ * - dynamische Verwaltung von Speicher muss mit malloc() und free()
+ *   durchgeführt werden; dynamische Arrays sind nicht erlaubt !!!
+ *--------------------------------------------------------------------*/
 Bool put(int zahl)
 {
-	List *neu;//Struct für die aktuelle Stelle anlegen
-	neu = malloc(SPEICHER); // Speicher reservieren 
+	List *neu;				// Struct für die aktuelle Stelle anlegen
+	neu = malloc(SPEICHER); // Speicher reservieren
 	if (neu == NULL)
 		return FALSE;
-	neu->nummer = zahl; // zahl in die Structure schreiben
-	neu->naechste = NULL;// Zeiger auf die nächste Stuctur auf 0 setzen
- 
-	if (anfang == NULL)  // Erstellen des Anfangzeigers
+	neu->nummer = zahl;	  // zahl in die Structure schreiben
+	neu->naechste = NULL; // Zeiger auf die nächste Stuctur auf 0 setzen
+
+	if (anfang == NULL) // Erstellen des Anfangzeigers
 		anfang = neu;
-	else                // Ab dem zweiten Aufruf adresse von neu in ende schreiben 
+	else // Ab dem zweiten Aufruf adresse von neu in ende schreiben
 		ende->naechste = neu;
 	ende = neu;
- 
+
 	return TRUE;
 }
- 
- 
+
 /*--------------------------------------------------------------------*\
-* Auslesen aus Liste
-* Parameter:
-*    keine
-* Return Wert:
-*    Zahl am Anfang der Liste oder aber QLEER, wenn Liste leer ist.
-*--------------------------------------------------------------------*/
+ * Auslesen aus Liste
+ * Parameter:
+ *    keine
+ * Return Wert:
+ *    Zahl am Anfang der Liste oder aber QLEER, wenn Liste leer ist.
+ *--------------------------------------------------------------------*/
 int get(void)
 {
 	if (isEmpty())
 		return QLEER;
 	int wert;
 	List *zeiger;
-	zeiger = anfang;     // zeiger zeigt auf Anfang von Array
+	zeiger = anfang;		   // zeiger zeigt auf Anfang von Array
 	anfang = anfang->naechste; // Neuer Anfang des Arrays um eine Struct weiter schieben
 	if (anfang == NULL)
 		ende = NULL;
-	wert = zeiger->nummer;   // hier wurde die erste Zahl des Arrays abgelegt (hier liegt die erste Struct)
-	free(zeiger);            // Speicher der ersten Struct wieder freigeben
+	wert = zeiger->nummer; // hier wurde die erste Zahl des Arrays abgelegt (hier liegt die erste Struct)
+	free(zeiger);		   // Speicher der ersten Struct wieder freigeben
 	return wert;
 }
- 
- 
+
 /*--------------------------------------------------------------------*\
-* Pruefen der Liste
-* Parameter:
-*    keine
-* Return Wert:
-*    liefert TRUE, wenn Queue leer ist, sonst FALSE
-*--------------------------------------------------------------------*/
+ * Pruefen der Liste
+ * Parameter:
+ *    keine
+ * Return Wert:
+ *    liefert TRUE, wenn Queue leer ist, sonst FALSE
+ *--------------------------------------------------------------------*/
 Bool isEmpty(void)
 {
 	return (anfang == NULL && ende == NULL);
